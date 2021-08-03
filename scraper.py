0da87b2124176226017a17b6a9526affb8e04956
EJ R <ejaramos@gmail.com>
2021-08-03 15:02:22 -0700

    starting dev for scraper

---
+++ scraper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""A simple python script template.
+"""
+
+import os
+import sys
+import argparse
+
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def get_url(url):
+    '''wget a url '''
+    return requests.get(url)
+
+def main(arguments):
+
+    # default parse args
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('url', help="Input url to scrape", type=argparse.FileType('r'))
+    parser.add_argument('-o', '--outfile', help="Output file",
+                        default=sys.stdout, type=argparse.FileType('w'))
+
+    args = parser.parse_args(arguments)
+
+    print(args)
+
+    # logic
+    # get the page
+    page = get_url(args['url'])
+
+    # parse the text
+
+    # create a dataframe
+
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
